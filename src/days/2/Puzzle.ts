719d9cfe43197fd6c0d7320d567f1233b233b046
Luciano Dias Varao <luciano@mytaxflow.dev>
2022-12-07 23:25:00 +0100

    reset move points

--- src/days/2/Puzzle.ts
+++ src/days/2/Puzzle.ts
@@ -6,61 +6,61 @@ export default class ConcretePuzzle extends Puzzle {
   public solveFirst(): string {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const fs = require('fs');
-    const array: any[] = [fs.readFileSync('./src/days/2/input.txt', 'utf8').toString().trim().split(/\s+/)];
-
-    console.log(array);
-    console.log(process.cwd());
+    const input: unknown[] = fs.readFileSync('./src/days/2/input.txt', 'utf8').toString().trim().split(/\s+/);
     let myTotalScore = 0;
-
     let myCurrentMovePoints = 0;
     let opponentCurrentMovePoints = 0;
-    array[0].forEach(function (currentMove: string) {
-      if (currentMove as string === 'A' || currentMove as string === 'B' || currentMove as string === 'C') {
-        switch (currentMove as string) {
-          case 'A': {
-            opponentCurrentMovePoints = 1;
-            break;
-          }
-          case 'B': {
-            opponentCurrentMovePoints = 2;
-            break;
-          }
-          case 'C': {
-            opponentCurrentMovePoints = 3;
-            break;
-          }
 
+    input.forEach(function (currentMove: string) {
+      opponentCurrentMovePoints = 0;
+      myCurrentMovePoints = 0;
+      switch (currentMove as string) {
+        case 'A': {
+          opponentCurrentMovePoints = 1;
+          break;
         }
-      } else {
-        switch (currentMove as string) {
-          case 'X': {
-            myCurrentMovePoints = 1;
-            break;
-          }
-          case 'Y': {
-            myCurrentMovePoints = 2;
-            break;
-          }
-          case 'Z': {
-            myCurrentMovePoints = 3;
-            break;
-          }
-
+        case 'B': {
+          opponentCurrentMovePoints = 2;
+          break;
+        }
+        case 'C': {
+          opponentCurrentMovePoints = 3;
+          break;
+        }
+        case 'X': {
+          myCurrentMovePoints = 1;
+          break;
+        }
+        case 'Y': {
+          myCurrentMovePoints = 2;
+          break;
+        }
+        case 'Z': {
+          myCurrentMovePoints = 3;
+          break;
         }
       }
+
       if (opponentCurrentMovePoints && myCurrentMovePoints) {
         if (myCurrentMovePoints > opponentCurrentMovePoints) {
           myTotalScore = myTotalScore + myCurrentMovePoints + 6;
+          console.log('win', myCurrentMovePoints, opponentCurrentMovePoints);
+          console.log(myTotalScore);
         } else if (opponentCurrentMovePoints > myCurrentMovePoints) {
           myTotalScore = myTotalScore + myCurrentMovePoints;
+          console.log('loss', myCurrentMovePoints, opponentCurrentMovePoints);
+          console.log(myTotalScore);
         } else if (opponentCurrentMovePoints === myCurrentMovePoints) {
           myTotalScore = myTotalScore + myCurrentMovePoints + 3;
+          console.log('tie', myCurrentMovePoints, opponentCurrentMovePoints);
+          console.log(myTotalScore);
         }
+        myCurrentMovePoints = 0;
+        opponentCurrentMovePoints = 0;
       }
     });
-
     console.log(myTotalScore);
-    return;
+    return `solution ${myTotalScore.toString()}`;
   }
 
   public getFirstExpectedResult(): string {
